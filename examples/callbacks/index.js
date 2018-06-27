b66266416ad14d2356c485ae4c341fa8a50992c1
YasharF <YasharF.Git1@ydftech.com>
2018-06-26 23:01:00 -0700

    Upgrade gulp-eslint to v4

--- examples/callbacks/index.js
+++ examples/callbacks/index.js
@@ -1,3 +1,4 @@
+'use strict';
 var mongoose = require('mongoose');
 
 var bookSchema = new mongoose.Schema({
--- examples/promises/test.js
+++ examples/promises/test.js
@@ -1,3 +1,4 @@
+'use strict';
 var assert = require('assert');
 var sinon = require('sinon');
 var mongoose = require('mongoose');
